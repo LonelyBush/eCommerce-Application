9f8242cc927cc12f0a386310ad2af6bb1fb605c1
LonelyBush <lonely.bush68@gmail.com>
2024-05-02 14:34:50 +0300

    feat: add Prettier config to the prod acc. RSS-ECOMM-1_11

--- eCommerce-App/.eslintrc.cjs
+++ eCommerce-App/.eslintrc.cjs
@@ -9,15 +9,14 @@ module.exports = {
     'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
+    'plugin:prettier/recommended',
   ],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
         sourceType: 'script',
       },
@@ -29,11 +28,14 @@ module.exports = {
     sourceType: 'module',
     project: ['./tsconfig.json', './tsconfig.node.json'],
   },
-  plugins: [
-    '@typescript-eslint',
-    'react',
-  ],
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
   rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
     'react/react-in-jsx-scope': 0,
   },
 };
