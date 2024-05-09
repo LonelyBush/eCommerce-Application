47ccb68615f23f9553515dd821c57c1225a34cdb
Yana-Dyachok <annetart1994@gmail.com>
2024-05-09 17:17:54 +0300

    feat: add animation and content to page

--- eCommerce-App/src/pages/not-found-page/not-found-page.tsx
+++ eCommerce-App/src/pages/not-found-page/not-found-page.tsx
@@ -1,10 +1,20 @@
 import { Link } from 'react-router-dom';
 import Button from '../../utils/button/button';
-//import styles from './not-found.module.css';
+import styles from './not-found.module.css';
 
 function NotFoundPage() {
   return (
     <div>
+      <h1> Ooops... Page not found</h1>
+      <section className={styles.errorContainer}>
+        <span>
+          <span>4</span>
+        </span>
+        <span>0</span>
+        <span>
+          <span>4</span>
+        </span>
+      </section>
       <Link to="/main">
         <Button btnType="button">To main page</Button>
       </Link>
@@ -13,3 +23,5 @@ function NotFoundPage() {
 }
 
 export default NotFoundPage;
+
+// the idea was taken from https://codepen.io/ricardpriet/pen/MOKEam
