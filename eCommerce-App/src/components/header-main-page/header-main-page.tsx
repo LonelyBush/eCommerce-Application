2da4ed10e6626de774cbac6675497d1150d496f7
Yana-Dyachok <annetart1994@gmail.com>
2024-05-19 19:42:32 +0300

    feat: add burger menu

--- eCommerce-App/src/components/header-main-page/header-main-page.tsx
+++ eCommerce-App/src/components/header-main-page/header-main-page.tsx
@@ -1,13 +1,37 @@
+import { useState } from 'react';
 import LinkTemplate from '../ui/link/link';
 import Logo from '../ui/logo/logo';
 
 import styles from './header-main-page.module.css';
 
 function HeaderMainPage() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className={styles.headerMain}>
       <Logo />
-      <nav className={styles.navMain}>
+      <button className={styles.burgerMenu} onClick={toggleMenu} type="button">
+        <div
+          className={
+            isMenuOpen
+              ? `${styles.burgerIcon} ${styles.open}`
+              : styles.burgerIcon
+          }
+        >
+          <span className={styles.spanBurger} />
+          <span className={styles.spanBurger} />
+          <span className={styles.spanBurger} />
+        </div>
+      </button>
+      <nav
+        className={
+          isMenuOpen ? `${styles.navMain} ${styles.open}` : styles.navMain
+        }
+      >
         <LinkTemplate to="/login">Log in</LinkTemplate>
         <LinkTemplate to="/registration">Sign up</LinkTemplate>
       </nav>
