d7e181d14d5bd2da17e7cf9a2f7c086bdaf143d5
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2024-05-21 23:17:13 +0300

    refactor/adaptive-logo-links (#32)
    * feat: add pages
    
    * fix: change div to header
    
    * fix: change logo

---
+++ eCommerce-App/src/pages/catalog-page/catalog-page.tsx
@@ -0,0 +1,12 @@
+import HeaderMainPage from '../../components/header-main-page/header-main-page';
+
+function CatalogPage() {
+  return (
+    <>
+      <HeaderMainPage />
+      <div> Catalog Page</div>
+    </>
+  );
+}
+
+export default CatalogPage;
