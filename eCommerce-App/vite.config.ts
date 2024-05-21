2ff3bb080147c57daf167506c2236247077bcf9f
Yana-Dyachok <annetart1994@gmail.com>
2024-05-21 23:57:14 +0300

    feat: add test for link

--- eCommerce-App/vite.config.ts
+++ eCommerce-App/vite.config.ts
@@ -5,7 +5,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
@@ -28,6 +27,10 @@ export default defineConfig(({ mode }) => {
       globals: true,
       environment: 'jsdom',
       setupFiles: ['./src/test_setup/setupTests.ts'],
+      coverage: {
+        provider: 'v8', 
+        reporter: ['text', 'json', 'html'], 
+      },
     },
     css: {
       modules: {},
