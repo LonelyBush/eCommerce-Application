a142845839bcfa63d59147fa41ac7e1642a40f1d
RS0048 <148986971+RS0048@users.noreply.github.com>
2024-05-25 13:57:37 +0300

    feat/create-unit-test (#35)
    * feat: add test for link
    
    * feat: add vite config
    
    ---------
    
    Co-authored-by: Yana-Dyachok <annetart1994@gmail.com>

--- eCommerce-App/src/App.test.tsx
+++ eCommerce-App/src/App.test.tsx
@@ -1,9 +1,16 @@
-import { test, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, World!/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  it('renders learn react link', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const linkElement = screen.getByRole('link', { name: /learn react/i });
+    expect(linkElement).toBeInTheDocument();
+  });
 });
