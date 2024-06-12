c70f1bd8b06a402f0a39e7ac6f85c79d202cd90e
Yana-Dyachok <annetart1994@gmail.com>
2024-06-12 19:37:38 +0300

    feat: add cart icon in product cart

--- eCommerce-App/src/components/header-main-page/header-main-page.tsx
+++ eCommerce-App/src/components/header-main-page/header-main-page.tsx
@@ -18,7 +18,7 @@ function HeaderMainPage() {
   const [countCart, setCountCart] = useState<number>(0);
 
   useEffect(() => {
-    setCountCart(10);
+    setCountCart(0);
   }); // запрос для получения количества елементов в корзине
 
   const toggleMenu = () => {
