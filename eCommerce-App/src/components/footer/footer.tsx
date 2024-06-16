6ce0ee1f5703f12d6faa55f296c4ca9a5991f352
Yana-Dyachok <annetart1994@gmail.com>
2024-06-16 20:11:10 +0300

    feat: add github's links

--- eCommerce-App/src/components/footer/footer.tsx
+++ eCommerce-App/src/components/footer/footer.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import LinkTemplate from '../ui/link/link';
 import styles from './footer.module.css';
 
 function Footer() {
@@ -8,10 +9,24 @@ function Footer() {
         <div className={styles.logoRS} />
       </Link>
       <p className={styles.year}>2024 &copy;</p>
-      <Link to="/about-us" className={styles.linkBlock}>
-        <div className={styles.iconGitHub} />
-        <span className={styles.text}>Our team</span>
-      </Link>
+      <div className={styles.teamBlock}>
+        <Link to="/about-us" className={styles.linkBlock}>
+          <div className={styles.iconGitHub} />
+          <span className={styles.text}>Our team</span>
+        </Link>
+        <nav className={styles.footerNav}>
+          {' '}
+          <LinkTemplate to="https://github.com/rs0048">
+            Roman Sokolov
+          </LinkTemplate>
+          <LinkTemplate to="https://github.com/Yana-Dyachok">
+            Yana Dyachok
+          </LinkTemplate>
+          <LinkTemplate to="https://github.com/lonelybush">
+            Nikita Radevich
+          </LinkTemplate>
+        </nav>
+      </div>
     </footer>
   );
 }
