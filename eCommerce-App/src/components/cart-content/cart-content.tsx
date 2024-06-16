83e8a92480e4ff88e469b6c04a45534bc8e952c9
LonelyBush <lonely.bush68@gmail.com>
2024-06-16 21:25:23 +0300

    feat: added cart-content static layout

--- eCommerce-App/src/components/cart-content/cart-content.tsx
+++ eCommerce-App/src/components/cart-content/cart-content.tsx
@@ -1,3 +1,4 @@
+import CartOrderList from '../cart-order-list/cart-order-list';
 import CartPriceView from '../cart-price-view/cart-price-view';
 import HistoryBack from '../ui/history-back/history-back';
 import Tags from '../ui/tags/tags';
@@ -11,9 +12,14 @@ export function CartContent() {
         <div className={styles.cartTitle}>
           <Tags.H2>Cart</Tags.H2>
           <p>3 Items</p>
+          <div className={styles.deleteAllBlock}>
+            <div className={styles.deleteAllIcon} />
+            <div className={styles.hide}>Delete All</div>
+          </div>
         </div>
 
         <div className={styles.mainCartView}>
+          <CartOrderList />
           <CartPriceView />
         </div>
       </div>
