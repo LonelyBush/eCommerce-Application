90097e1a84a84d4ad8100cb40ac47d668cb92ad4
Yana-Dyachok <annetart1994@gmail.com>
2024-06-17 21:08:59 +0300

    feat: add main content

--- eCommerce-App/src/pages/cart-page/cart-page.tsx
+++ eCommerce-App/src/pages/cart-page/cart-page.tsx
@@ -1,11 +1,13 @@
 import { CartContent } from '../../components/cart-content/cart-content';
 import HeaderMainPage from '../../components/header-main-page/header-main-page';
+import Footer from '../../components/footer/footer';
 
 function CartPage() {
   return (
     <>
       <HeaderMainPage />
       <CartContent />
+      <Footer />
     </>
   );
 }
